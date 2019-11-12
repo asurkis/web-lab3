914e799e649b47d0b16a62c595d119e1136411c1
Anton Surkis <antonsurkis@gmail.com>
2019-11-12 18:07:37 +0300

    Refactoring

--- src/main/java/ru/ifmo/se/labs/asurkis/lab3/Point.java
+++ src/main/java/ru/ifmo/se/labs/asurkis/lab3/Point.java
@@ -1,15 +1,44 @@
 package ru.ifmo.se.labs.asurkis.lab3;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Embeddable
 public class Point implements Serializable {
     private double x;
     private double y;
 
+    public Point() {
+        this(0, 0);
+    }
+
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public boolean fallsInto(double r) {
+        if (x > 0) {
+            if (y > 0) {
+                return x * x + y * y <= r * r;
+            } else if (y < 0) {
+                return x - y <= 0.5 * r;
+            } else {
+                return x <= r;
+            }
+        } else if (x < 0) {
+            if (y > 0) {
+                return -r <= x && y <= 0.5 * r;
+            } else if (y < 0) {
+                return false;
+            } else {
+                return -r <= x;
+            }
+        } else {
+            return -0.5 * r <= y && y <= r;
+        }
     }
 
     @Override
@@ -34,33 +63,21 @@ public class Point implements Serializable {
         return Objects.hash(x, y);
     }
 
+    @Column(name = "query_point_x")
     public double getX() {
         return x;
     }
 
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    @Column(name = "query_point_y")
     public double getY() {
         return y;
     }
 
-    public boolean fallsInto(double r) {
-        if (x > 0) {
-            if (y > 0) {
-                return x * x + y * y <= r * r;
-            } else if (y < 0) {
-                return x - y <= 0.5 * r;
-            } else {
-                return x <= r;
-            }
-        } else if (x < 0) {
-            if (y > 0) {
-                return -r <= x && y <= 0.5 * r;
-            } else if (y < 0) {
-                return false;
-            } else {
-                return -r <= x;
-            }
-        } else {
-            return -0.5 * r <= y && y <= r;
-        }
+    public void setY(double y) {
+        this.y = y;
     }
 }
--- src/main/java/ru/ifmo/se/labs/asurkis/lab3/Query.java
+++ src/main/java/ru/ifmo/se/labs/asurkis/lab3/Query.java
@@ -1,15 +1,27 @@
 package ru.ifmo.se.labs.asurkis.lab3;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "queries")
 public class Query implements Serializable {
+    private int id;
     private Point point;
     private double radius;
+
+    public Query() {
+        this(new Point(), 0);
+    }
 
     public Query(Point point, double radius) {
         this.point = point;
         this.radius = radius;
+    }
+
+    public boolean evaluateResult() {
+        return point.fallsInto(radius);
     }
 
     @Override
@@ -34,10 +46,17 @@ public class Query implements Serializable {
         return Objects.hash(point, radius);
     }
 
-    public boolean getResult() {
-        return point.fallsInto(radius);
+    @Id @Column(name = "query_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "query_point")
     public Point getPoint() {
         return point;
     }
@@ -46,6 +65,7 @@ public class Query implements Serializable {
         this.point = point;
     }
 
+    @Column(name = "query_radius")
     public double getRadius() {
         return radius;
     }
