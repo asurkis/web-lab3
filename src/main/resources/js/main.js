9e043b3d1a437da5b898a88ff4cac4d97acf7c7d
Anton Surkis <antonsurkis@gmail.com>
2019-11-15 11:54:32 +0300

    Changes in class

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -1,16 +1,11 @@
 'use strict';
-let svg, svgInputX, svgInputY, checkButton;
-
-window.addEventListener('load', function() {
-    svg = document.querySelector('svg');
-    svgInputX = document.getElementById('pictureForm:svgInputX');
-    svgInputY = document.getElementById('pictureForm:svgInputY');
-    checkButton = document.getElementById('pictureForm:checkButton');
-
-    svg.addEventListener('click', onPictureClick);
-});
 
 function onPictureClick(e) {
+    const svg = document.querySelector('svg');
+    const svgInputX = document.getElementById('pictureForm:svgInputX');
+    const svgInputY = document.getElementById('pictureForm:svgInputY');
+    const checkButton = document.getElementById('pictureForm:checkButton');
+
     const rect = svg.getBoundingClientRect();
     const x = 0.02 * (e.x - rect.left - 300);
     const y = -0.02 * (e.y - rect.top - 300);
